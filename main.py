09588b310e6257f40083fa1c093c3cb65f16fa04
TannerBebb <17tbebb@gmail.com>
2025-06-17 09:23:18 -0500

    I added objectives to the top of the source code(June 17th 2025 09:23.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 """
     Initial setup - June 17th 2025 0830
+    Python Calculator Project
+        Basic arethemitic functions (add, subtract, multiply, divide)
 """
+
+"""
+    1.) Ask the user what they would like to do.
+    2.) Ask the user for 2 variables.
+    3.) Preform selected operation with variables from step 2.
+    4.) Display the result to the user.
+    5.) Ask the user if they would like to run the program again (Optional).
+"""
+
