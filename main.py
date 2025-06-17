5ffa31e08ca3263cdaf2a7878186c918b326037f
TannerBebb <17tbebb@gmail.com>
2025-06-17 10:47:37 -0500

    I finshed the multiplication function. I will be working on the division funciton next(June 17th 2025 10:47).

--- main.py
+++ main.py
@@ -40,3 +40,13 @@ elif userChoice == "S" or userChoice == "s":
 
     result = integerX - integerY
     print(result)
+
+elif userChoice == "M" or userChoice == "m":
+    stringX = input("Please enter a number for x: ")
+    integerX = int(stringX)
+    
+    stringY = input("Please enter a number for y: ")
+    integerY = int(stringY)
+
+    result = integerX * integerY
+    print(result)
